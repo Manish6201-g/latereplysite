c7fce213a8027f047cfb15ba65371d2844298148
Manish6201-g <245906761+Manish6201-g@users.noreply.github.com>
2026-08-26 14:50:53 +0530

    Integrate 3D Coverflow Carousel component and setup TypeScript

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,10 @@
+import Demo from "@/components/demo";
+
+export const metadata = {
+  title: "3D Coverflow Carousel Demo",
+  description: "3D Coverflow Carousel component demo",
+};
+
+export default function DemoPage() {
+  return <Demo />;
+}
